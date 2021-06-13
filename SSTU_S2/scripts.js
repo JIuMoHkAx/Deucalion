23b9760a8402cecba14b49d8c0a8440a0258e7a9
JIuMoHkA <JIuMoHkA99@mail.ru>
2021-06-14 03:57:47 +0400

    Залил на гит

---
+++ SSTU_S2/scripts.js
@@ -0,0 +1,8 @@
+function mainbutton_click(){
+    document.getElementById("anketid").style.display="none";
+    document.getElementById("contid").style.display="block";   
+}
+function formbutton_click(){
+    document.getElementById("contid").style.display="none";
+    document.getElementById("anketid").style.display="block";   
+}
